09432179e196133468743b2b9cb2ce52d4ec4248
Areej Anwaar <231969@students.au.edu.pk>
2026-03-30 16:27:28 +0500

    Final cleanup, metadata update, and animation restoration

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -38,18 +38,10 @@ import karma from "./company/karma.png";
 import alkhidmat from "./company/alkhidmat.png";
 import auaws from "./company/auaws.png";
 
-import project1 from "./projects/project1.png";
-import project2 from "./projects/project2.png";
-import project3 from "./projects/project3.png";
-import project4 from "./projects/project4.png";
-import project5 from "./projects/project5.png";
-import project6 from "./projects/project6.png";
 import cyberpunkVideo from "./projects/cyberpunk_video.mp4";
 import servify_vid from "./projects/servify_vid.mp4";
 import codeairCert from "./projects/codeair.jpg";
-import internship_1 from "./internship_1.jpg";
-import internship_2 from "./internship_2.jpg";
-import vounterwork_certificate from "./vounterwork_certificate.jpeg";
+
 
 import pizza1 from "./projects/pizza1.jpg";
 import pizza2 from "./projects/pizza2.jpg";
@@ -61,9 +53,7 @@ import game3 from "./projects/game3.png";
 import game4 from "./projects/game4.png";
 import cpuScheduler from "./projects/cpu_scheduler.jpeg";
 
-import user1 from "./testimonials/user1.jpg";
-import user2 from "./testimonials/user2.jpg";
-import user3 from "./testimonials/user3.jpg";
+
 
 import github from "./socials/github.svg";
 import linkedin from "./socials/linkedin.svg";
@@ -101,14 +91,6 @@ export {
   karma,
   alkhidmat,
   auaws,
-  project1,
-  project2,
-  project3,
-  project4,
-  project5,
-  project6,
-  user2,
-  user3,
   youtube,
   linkedin,
   twitter,
@@ -130,7 +112,4 @@ export {
   game3,
   game4,
   cpuScheduler,
-  internship_1,
-  internship_2,
-  vounterwork_certificate,
 };
